fddef886c18a95b1fb3d403e95be704105c46f4e
JOY DEEP SAHA <88198830+sahajoydeep2002@users.noreply.github.com>
2022-05-19 22:33:13 +0530

    Delete src directory

--- src/Components/Container/index.tsx
+++
@@ -1,9 +0,0 @@
-import { StyledContainer } from "./styles";
-export interface ContainerProps {
-    children: React.ReactNode;
-}
-const Container = ({ children }: ContainerProps) => (
-  <StyledContainer>{children}</StyledContainer>
-);
-
-export default Container;
